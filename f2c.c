27e43ed2283ce4a3c8c4e643f063e9dcd8bad56d
Tianbo <tianbo@hey.com>
2022-12-28 20:35:18 -0800

    f2c

---
+++ f2c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define LOWER 0   /* lower limit of table */
+#define UPPER 300 /* upper limit of table */
+#define STEP 20   /* step size */
+
+/* print Fahrenheit-Celsius table */
+int main() {
+  printf("%3c %6c\n", 'F', 'C');
+  for (int fahr = LOWER; fahr <= UPPER; fahr += STEP)
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+}
