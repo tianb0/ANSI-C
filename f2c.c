c2070a978fa82a8d51de15ea9db125310cb4ea1f
Tianbo <tianbo@hey.com>
2022-12-28 20:54:16 -0800

    format

--- f2c.c
+++ f2c.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#define LOWER 0   /* lower limit of table */
-#define UPPER 300 /* upper limit of table */
-#define STEP 20   /* step size */
+#define    LOWER   0      /* lower limit of table */
+#define    UPPER   300    /* upper limit of table */
+#define    STEP    20     /* step size */
 
 /* print Fahrenheit-Celsius table */
 int main() {
