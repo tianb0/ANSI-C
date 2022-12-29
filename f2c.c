fb59b065c068f600af210a0413e0835ca9f06593
Tianbo <tianbo@hey.com>
2022-12-29 09:43:00 -0800

    refactor

--- f2c.c
+++ f2c.c
@@ -6,7 +6,8 @@
 
 /* print Fahrenheit-Celsius table */
 int main() {
+  int fahr;
   printf("%3c %6c\n", 'F', 'C');
-  for (int fahr = LOWER; fahr <= UPPER; fahr += STEP)
+  for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
     printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
