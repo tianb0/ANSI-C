b9eaa7e4a9c9f4a8f11780c4cb2e9c08bd37983a
Tianbo <tianbo@hey.com>
2023-03-14 09:47:02 -0700

    rpc

---
+++ rpc.c
@@ -0,0 +1,130 @@
+/* reverse Polish calculator */
+#include <stdio.h>
+#include <stdlib.h> /* for atof() */
+
+#define MAXOP 100 /*  max size of operand or operator */
+#define NUMBER '0'
+
+int getop(char[]);
+void push(double);
+double pop(void);
+
+int main() {
+  int type;
+  double op2;
+  char s[MAXOP];
+
+  while ((type = getop(s)) != EOF) {
+    switch (type) {
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if (op2 != 0.0)
+          push(pop() / op2);
+        else
+          printf("error: zero divisor\n");
+        break;
+      case '\n':
+        printf("\t%.8g\n", pop());
+        break;
+      default:
+        printf("error: unknown command %s\n", s);
+        break;
+    }
+    return 0;
+  }
+}
+
+#define MAXVAL 100 /* maximum depth of val stack */
+
+int sp = 0; /* next free stack position */
+double val[MAXVAL]; /* value stack */
+
+/* push: push f onto value stack */
+void push(double f) {
+  if (sp < MAXVAL)
+    val[sp++] = f;
+  else
+    printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void) {
+  if (sp > 0)
+    return val[--sp];
+  else {
+    printf("error: stack empty\n");
+    return 0.0;
+  }
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next operator or numeric operand */
+int getop(char s[]) {
+  int i, c;
+
+  while ((s[0] = c = getch()) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+  if (!isdigit(c) && c != '.')
+    return c; /* not a number */
+  i = 0;
+  if (isdigit(c)) /* collect integer part */
+    while (isdigit(s[++i] = c = getch()))
+      ;
+  if (c == '.') /* collect fraction part */
+    while (isdigit(s[++i] = c = getch()))
+      ;
+  s[i] = '\0';
+  if (c != EOF)
+    ungetch(c);
+  return NUMBER;
+}
+
+/**
+* " 12.34\n"
+* s[0] = c = ' '
+* s[0] = c = '1'
+* s[1] = '\0'
+* i = 0
+* i = 1, s[1] = c = '2'
+* i = 2, s[2] = c = '.'
+* i = 3, s[3] = c = '3'
+* i = 4, s[4] = c = '4'
+* i = 5, s[5] = c = '\n'
+* s[i] = '\0'
+*/
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+/* get a possibly pushed back character */
+int getch(void) {
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* push character back on input */
+void ungetch(int c) {
+  if (bufp >= BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
+}
