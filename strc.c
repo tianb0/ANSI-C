16ed619a2c9e0f4245275380f585c9593c3e0b1a
Tianbo <tianbo@hey.com>
2023-04-02 12:33:51 -0700

    strcmp

--- strc.c
+++ strc.c
@@ -3,3 +3,11 @@ void strc(char *s, char *t) {
   while (*s++ = *t++) // while ((*s++ = *t++) != '\0')
     ;
 }
+
+// returns negative, zero or positive if s is lexicographically less than, equal to, or greater than 0
+int strcmp1(char *s, char *t) {
+  for (; *s == *t; s++, t++) 
+    if (*s == '\0')
+      return 0;
+  return *s - *t;
+}
