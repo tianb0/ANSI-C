15f6575178e44d6e9a969161c4c6a8c0fa8bf024
Tianbo <tianbo@hey.com>
2023-04-02 12:28:52 -0700

    strcpy

---
+++ cpf.c
@@ -0,0 +1,5 @@
+// an array
+char amessage[] = "now is the time";
+
+// a pointer
+char *pmessage = "now is the time";
---
+++ strc.c
@@ -0,0 +1,5 @@
+// copy t to s
+void strc(char *s, char *t) {
+  while (*s++ = *t++) // while ((*s++ = *t++) != '\0')
+    ;
+}
